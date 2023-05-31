3da16d3e3aa45821611b560beb3b72e314095391
Khurshed-M <master.khurshed@gmail.com>
2023-05-31 22:22:15 +0300

    delete blog, edit blog functions added

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,9 +1,8 @@
 const jwt = require('jsonwebtoken');
-const blog = require("../models/blog.model");
+const Blog = require("../models/blog.model");
 const User = require("../models/user.model");
 
 module.exports.dashboard_get = async (req, res) => {
-
     const token = req.cookies.jwt;
     const decoded = jwt.verify(token, 'moy sekret');
     let user = await User.findById(decoded.param).then(foundUser=>{
@@ -11,7 +10,31 @@ module.exports.dashboard_get = async (req, res) => {
     });
     const author = user;
 
-    blog.find({ author }).then((blogs) => {
+    Blog.find({ author }).then((blogs) => {
         res.render("dashboard", { blogs });
     });
 }
+
+module.exports.dashboard_delete = async (req, res) => {
+    const blogId = req.params._id;
+    await Blog.deleteOne({ _id: blogId });
+    res.send('done');
+ }
+
+module.exports.edit_get = async (req, res) => {
+    const blogId = req.params._id;
+    const foundBlog = await Blog.findOne({ _id: blogId });
+    res.render("edit", { title: foundBlog.title, content: foundBlog.content, id: foundBlog._id });
+}
+
+module.exports.edit_put = async (req, res) => {
+    
+    const blogId = req.params._id;
+    const { title, content } = req.body;
+    try {
+        const text = await Blog.updateOne({ _id: blogId }, {$set: { title, content }});
+        res.json(text);
+    } catch (error) {
+        console.log(error);
+    }
+ }
--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -5,5 +5,8 @@ const { verifyAuth } = require('../middleware/authMiddleware');
 const router = Router();
 
 router.get('/dashboard', verifyAuth, dashboardController.dashboard_get);
+router.delete('/dashboard/delete/:_id', verifyAuth, dashboardController.dashboard_delete);
+router.get('/dashboard/edit/:_id', verifyAuth, dashboardController.edit_get);
+router.put('/dashboard/edit/:_id', verifyAuth, dashboardController.edit_put);
 
 module.exports = router;
